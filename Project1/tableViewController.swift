ed65ad86c3e9a3d8994822cd776dd193d2248256
Lydia <guipopo5@gmail.com>
2024-02-24 12:55:13 +0800

    Demo Init

--- Project1/tableViewController.swift
+++ Project1/tableViewController.swift
@@ -13,6 +13,9 @@ class tableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Init
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -27,6 +30,22 @@ class tableViewController: UITableViewController {
         
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+         vc.selectedImage = pictures[indexPath.row]
+         navigationController?.pushViewController(vc, animated: true)
+         }
+    }
+    
 }
 
